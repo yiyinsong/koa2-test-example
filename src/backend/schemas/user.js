3881f7d6d8aaceb3002a60cc55eb26e87beada8f
yiyinsong <390396674@qq.com>
2017-11-20 18:26:37 +0800

    2017-11-20

---
+++ src/backend/schemas/user.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    tel: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    image: String,
+    meta: {
+        createTime: {
+            type: Date,
+            default: Date.now()
+        },
+        updateTime: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+});
+
+UserSchema.pre('save', (next) => {
+    if(this.isNew) {
+        this.meta.createTime = this.meta.updateTime = Date.now();
+    } else {
+        this.meta.updateTime = Date.now();
+    }
+});
+
+export default UserSchema;
